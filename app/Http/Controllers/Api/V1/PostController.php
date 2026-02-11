adb122e0ef96a9b355bd3af409cfb1016df33969
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2026-02-11 14:10:20 +0200

    Make the post model; Create the required files for the Api

---
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+  /**
+   * This function runs automatically when the Controller is called
+   */
+  public function __invoke()
+  {
+    $posts = Post::all();
+    // return $posts;
+
+    // this for only one post ('One record')
+    // return new PostResource($posts);
+
+    // But for a collection of posts
+    return PostResource::collection($posts);
+  }
+}
---
+++ app/Http/Resources/V1/PostResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+  /**
+   * Transform the resource into an array.
+   *
+   * @return array<string, mixed>
+   */
+  public function toArray(Request $request): array
+  {
+    return [
+      'count' => $this->count(),
+      "id" => $this->id,
+      "title" => $this->title,
+      "body" => $this->body,
+      "createdAt" => $this->created_at,
+      "updatedAt" => $this->updated_at,
+    ];
+  }
+}
