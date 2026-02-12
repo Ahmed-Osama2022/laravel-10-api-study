2537d8e409b563e1e6a4feb1e37ee14abe6b7401
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2026-02-12 09:10:39 +0200

    Add Comment model, controller, requests, policy, factory, migration, and seeder

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,18 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Comment extends Model
 {
   use HasFactory;
 
   protected $fillable = [
-    'title',
-    'body'
+    'body',
+    'post_id',
   ];
 
 
-  public function comments()
+
+  public function post()
   {
-    return $this->hasMany(Comment::class);
+    return $this->belongsTo(Post::class);
   }
 }
